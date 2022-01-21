663a1eb673d2a470196c7946245b29546b677590
Kher <ckkher@147dda3bad26.ant.amazon.com>
2022-01-22 00:58:15 +0530

    Created basic components for add, filter, delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
-class HelloWorld extends React.Component {
+class IssueFilter extends React.Component{
     render() {
-        const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-        const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-        const message = helloContinents.join(' ');
         return (
-            <div title="Outer Div">
-                <h1>{message}</h1>
-            </div>
-        );
+            <div>This is a placeholder for the issue filter.</div>
+        )
     }
 }
 
-const element = <HelloWorld />
-ReactDOM.render(element, document.getElementById("contents"));
+class IssueTable extends React.Component{
+    render() {
+        return (
+            <div>This is a placeholder for the issue table.</div>
+        )
+    }
+}
+
+class IssueAdd extends React.Component{
+    render() {
+        return (
+            <div>This is a placeholder for the issue add.</div>
+        )
+    }
+}
+
+class IssueList extends React.Component{
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </React.Fragment>
+        )
+    }
+}
+
+const element = <IssueList />;
+
+React.createElement(element, document.getElementById('contents'));
